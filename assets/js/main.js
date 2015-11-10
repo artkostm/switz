30548c1f17588fad022ad252791ba9604cdd8f56
Artsiom <artkostm@gmail.com>
2015-11-10 11:29:19 +0300

    Updated data preparation: replacing new line characters with <br> tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.get("https://raw.githubusercontent.com/artkostm/test/master/README.md", 
 				function(data)
 				{
-					$("#container_text").html(data);
+					$("#container_text").html(data.replace(/(\r\n|\n|\r)/gm, "<br>"));
 				}
 			);
 }); 
