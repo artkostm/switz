3ccd8bdc58d565699b4b97c4e6e02ee76e70499c
Artsiom <artkostm@gmail.com>
2017-04-23 19:58:09 +0300

    small fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $.get("https://raw.githubusercontent.com/artkostm/test/master/README.md", function(data) {
-		$("#container_text").html(data.replace(/(\r\n|\n|\r)/gm, "<br>"));
+$(function () {
+	$(document).ready(function(){
+	    $.get("https://raw.githubusercontent.com/artkostm/test/master/README.md", function(data) {
+			$("#container_text").html(data.replace(/(\r\n|\n|\r)/gm, "<br>"));
+		});
 	});
-}); 
+});
